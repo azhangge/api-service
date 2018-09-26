295e2b4a638a5a8512c77ea6789564933bbce6ad
azhangge <1007009258@qq.com>
2018-09-26 17:26:55 +0800

    B

--- src/main/java/com/huajie/dao/WalterDao.java
+++ src/main/java/com/huajie/dao/WalterDao.java
@@ -1,11 +1,8 @@
 package com.huajie.dao;
 
 import com.huajie.entity.vo.WalterVo;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface WalterDao {
     List<WalterVo> findByName(String name);
 }
