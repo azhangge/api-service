44f2ed3e81a394836adcbbb3b95cc1dd3b009504
azhangge <1007009258@qq.com>
2018-10-08 16:00:04 +0800

    spring boot 2.0 类重写过时

--- src/main/java/com/huajie/configs/WebConfig.java
+++ src/main/java/com/huajie/configs/WebConfig.java
@@ -5,26 +5,24 @@ import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by fangxing on 2017/7/8.
  */
 @EnableWebMvc
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
         registry.addResourceHandler("/*.ico").addResourceLocations("/");
-        super.addResourceHandlers(registry);
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new WebInterceptor());
-        super.addInterceptors(registry);
     }
 
 }
